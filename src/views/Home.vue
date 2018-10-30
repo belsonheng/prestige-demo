7e8f852105a1877bbbe3fa250cb03db154948a5c
Heng Belson <ccehb@nus.edu.sg>
2018-10-30 18:14:30 +0800

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="main">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <p>Main Page Here</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "home",
+  data: {
+
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Platform.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="platform">
+    <div class="carousell">
+      Carousell
+    </div>
+    <div class="grab">
+      Grab
+    </div>
+    <div class="shoppee">
+      Shoppee
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.platform {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  height: 100%;
+}
+.platform > div {
+  flex: 1;
+  flex-direction: column;
+}
+.carousell {
+  background-color: red;
+}
+.grab {
+  background-color: green;
+}
+.shoppee {
+  background-color: orange;
+}
+</style>
