525946d459803513e5c7e3344fcd2703d6a4fd16
Belson Heng <belsonheng@gmail.com>
2018-11-02 00:39:34 +0800

    cleanup

--- src/main.js
+++ src/main.js
@@ -2,13 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import VueFire from 'vuefire';
-import firebase from 'firebase/app'
-import 'firebase/firestore'
 
 Vue.use(VueFire);
-export const db = firebase.initializeApp({
-  databaseURL: 'https://is5003-platform.firebaseio.com/'
-}).database();
 
 Vue.config.productionTip = false;
 
